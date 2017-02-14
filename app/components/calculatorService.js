5ebbbf3a1b35968791f4c9421dfd26430e203328
bfnessar <bfnessar@email.wm.edu>
2017-02-14 16:33:57 -0500

    Two hours in. Decided to use infix notation and stack to implement calculator. Maybe I'm making this too complicated.

--- app/components/calculatorService.js
+++ app/components/calculatorService.js
@@ -13,10 +13,69 @@
         var myself = this;
 
         // Validate Input
-        var token_array = string.split("");
+        var invalid_chars = /[^\d*\+\-\*\/\s]/;
+        if (string.match(invalid_chars)) {
+          console.log("invalid chars");
+          return false;
+        }
+        else {
+          console.log("characters are valid!");
+        }
+
+        // Splits the string into single characters (multi-digit numbers are not yet grouped)
+        var trimmed_string = string.replace(/\s+/g, "");
+        var char_array = trimmed_string.split("");
+        console.log(char_array);
+
+        var token_array = [];
+        var current_token = '';
+        var current_char = '';
+
+        // Iterate over each character in the condensed expression
+        for (var i=0; i<char_array.length; i++) {
+          current_char = char_array[i];
+
+          if (current_char.match(/[\d]/)) {
+            current_token += current_char;
+            if (i==char_array.length-1) {
+              token_array.push(current_token);
+            }
+          }
+
+          else if (current_char.match(/[\+\-\/\*]/)) {
+            token_array.push(current_token);
+            token_array.push(current_char);
+            current_token = '';
+          }
+        }
+
         console.log(token_array);
 
         // Calculate
+        // Convert expressin into postfix. See http://csis.pace.edu/~wolf/CS122/infix-postfix.htm
+        var operator_stack = [];
+        var postfix_list = [];
+        for (var i=0; i<token_array.length; i++) {
+          var current_token = token_array[i];
+          if (current_token.match(/d+/)){
+            postfix_list.push(current_token);
+          }
+
+          else if (current_token.match(/[\*\/]/)){
+
+          }
+
+          else if (current_token.match(/[\+\-]/)) {
+            if (operator_stack[operator_stack.length].match(/[\*\/]/)){
+              postfix_list.push( operator_stack.pop() );
+            }
+            else {
+              operator_stack.push(current_token);
+            }
+          }
+        }
+
+
 
 
         // Output
